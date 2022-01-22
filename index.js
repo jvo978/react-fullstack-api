9c09d58f81219c52f6c6e7c8dd2924a23b19cb92
Jason Vo <jasonv.vo@gmail.com>
2022-01-22 17:27:37 -0500

    fix deployment bugs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express'); // instance of the framework
 const app = express(); // initialize our server, make api requests
 const cors = require('cors');
+require("dotenv").config();
 
 
 app.use(express.json())
@@ -19,8 +20,10 @@ app.use('/auth', usersRouter)
 const likesRouter = require('./routes/Likes');
 app.use('/likes', likesRouter)
 
-db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
+db.sequelize
+    .sync()
+    .then(() => {
+        app.listen(process.env.PORT || 3001, () => {
         console.log('Server is running on port 3001.')
     })
 })
